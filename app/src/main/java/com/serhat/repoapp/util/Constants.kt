98bc1ce56a05d1911f65a8f3aac0505fbf5d6f99
serhatozturk <serhat.ozturk@magis.com.tr>
2020-08-14 20:36:04 +0300

    Constant class created to store base url , duration etc. data class created to store user repository datas.

---
+++ app/src/main/java/com/serhat/repoapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.serhat.repoapp.util
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://api.github.com/"
+        const val REQUEST_TIMEOUT_DURATION = 30
+    }
+}
