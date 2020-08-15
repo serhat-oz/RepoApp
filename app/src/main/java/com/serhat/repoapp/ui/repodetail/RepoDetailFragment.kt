1eab61edbb0ec4ff8fba9e9144257841c62daa43
serhatozturk <serhat.ozturk@magis.com.tr>
2020-08-16 00:38:08 +0300

    RepoDetailFragment data's shown

--- app/src/main/java/com/serhat/repoapp/ui/repodetail/RepoDetailFragment.kt
+++ app/src/main/java/com/serhat/repoapp/ui/repodetail/RepoDetailFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.serhat.repoapp.R
+import com.serhat.repoapp.model.User
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_repo_detail.*
 
 class RepoDetailFragment: Fragment() {
 
@@ -17,5 +20,15 @@ class RepoDetailFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         var repoData = arguments?.let { RepoDetailFragmentArgs.fromBundle(it).repoData}
+
+        initViews(repoData)
+    }
+
+    private fun initViews(repoData: User?) {
+        txtUserName.text = repoData?.owner!!.login
+        Picasso.get().load(repoData.owner.avatar_url).placeholder(R.drawable.place_holder).into(ivUserImage)
+        //TODO format texts
+        tvStarCount.text = "Star Count: " + repoData.stargazers_count
+        tvOpenIssueCount.text = "Open Issues Count: " + repoData.open_issues_count
     }
 }
